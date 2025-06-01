e8a19f5d368c260fa60ddc4670c191cb4bf8faf9
Tronchedecake <doc.golshani@gmail.com>
2025-06-01 15:11:57 +0200

    message

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import MainContent from "../components/MainContent";
 import Footer from "../components/Footer";
-
+import Accueil from "../components/Accueil";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      coucou
+      <Accueil />
       <MainContent />
       <Footer />
     </div>
---
+++ src/components/Accueil.js
@@ -0,0 +1,8 @@
+
+
+
+export default function Accueil() {
+  return (
+    <div>Blabla</div>
+  );
+}
